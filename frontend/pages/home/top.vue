5953e60975b22077c8a710fce404519ca4193cc7
tmasuyama <tmasuyama1114@gmail.com>
2022-02-13 09:34:00 +0900

    :construction: Home#top用のVueを準備

---
+++ frontend/pages/home/top.vue
@@ -0,0 +1,20 @@
+<!--ページコンポーネント-->
+<!--ページコンポーネントでは使用する普通のコンポーネント（ここではApp）を読み込んで使用できる-->
+<template lang="pug">
+#home-top
+  App
+  p Top page
+  img(src='../../images/image.jpg')
+</template>
+
+<style lang="scss"></style>
+
+<script>
+import App from '../../components/app'
+
+export default {
+  components: {
+    App
+  }
+}
+</script>
