17b4c433bbab5c4eb767cda1d6b884d8c88d1531
tmasuyama <tmasuyama1114@gmail.com>
2022-02-13 10:02:29 +0900

    Revert ":construction: 画像を表示できるようにプロキシ設定"
    This reverts commit 87ebbf286337363a75b45439b5eb01365bdfd4c4.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.middleware.use AssetsPathProxy, ssl_verify_none: true
 end
