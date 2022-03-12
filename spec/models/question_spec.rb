fd8754202551e2e3bf6a2fa1151f8f929435da3c
tmasuyama <tmasuyama1114@gmail.com>
2022-03-12 11:56:14 +0900

    :construction: rubocopルールを追加

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  describe 'バリデーション' do
+  describe 'validation' do
     subject { question.valid? }
 
     let(:text) { 'てすと' }
     let(:question) { described_class.new(content: text) }
 
     it { expect(subject).to eq(true) }
-    
-    context 'contentがnilの場合' do
+
+    context 'when the content is nil' do
       let(:text) { nil }
+
       it { expect(subject).to eq(false) }
     end
   end
